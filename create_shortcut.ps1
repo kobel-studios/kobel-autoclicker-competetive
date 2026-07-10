0a70f53cf57d4f753f254f47b190600371d721f5
KOBEL <SladeJJ54@gmail.com>
2026-07-09 23:18:08 -0700

    Add icon index to shortcut for proper display

--- create_shortcut.ps1
+++ create_shortcut.ps1
@@ -4,7 +4,7 @@ $Shortcut = $WshShell.CreateShortcut("$Desktop\Kobel Autoclicker.lnk")
 $Shortcut.TargetPath = "pythonw.exe"
 $Shortcut.Arguments = "`"C:\Users\jacks\CascadeProjects\autoclicker-hub\autoclicker_hub.py`""
 $Shortcut.WorkingDirectory = "C:\Users\jacks\CascadeProjects\autoclicker-hub"
-$Shortcut.IconLocation = "C:\Users\jacks\CascadeProjects\autoclicker-hub\kobel_icon.ico"
+$Shortcut.IconLocation = "C:\Users\jacks\CascadeProjects\autoclicker-hub\kobel_icon.ico,0"
 $Shortcut.Description = "Kobel Autoclicker - Competitive Clicking"
 $Shortcut.Save()
 Write-Host "Shortcut created on Desktop"
